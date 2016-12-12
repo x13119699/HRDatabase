9450c79f58bbe43469c0b4acbb0dc38adf0f0c30
x15010899 <x15010899@student.ncirl.ie>
2016-12-12 17:21:09 -0500

    final change Francesco

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 //https://github.com/InspektorKukeke/USERDATABASE in order to delete a record with the splice record.
 //https://github.com/mikhail-nci/wad-simple-demo in order to make the get and post requests and to add a record to the table.
 
-
 var http = require('http'),
   path = require('path'),
   express = require('express'),
@@ -47,136 +46,62 @@ router.get('/get/html', function(req, res) {
 
 // POST request to add to JSON & XML files
 router.post('/post/json', function(req, res) {
-
   // Function to read in a JSON file, add to it & convert to XML
   function appendJSON(obj) {
-
     // Read in a JSON file
     var JSONfile = fs.readFileSync('Staff.json', 'utf8');
-
     // Parse the JSON file in order to be able to edit it 
     var JSONparsed = JSON.parse(JSONfile);
-
     // Add a new record into country array within the JSON file    
     JSONparsed.member.push(obj);
-
     // Beautify the resulting JSON file
     var JSONformated = JSON.stringify(JSONparsed, null, 4);
-
     // Write the updated JSON file back to the system 
     fs.writeFileSync('Staff.json', JSONformated);
-
     // Convert the updated JSON file to XML     
     var XMLformated = js2xmlparser.parse('staff', JSON.parse(JSONformated));
-
     // Write the resulting XML back to the system
     fs.writeFileSync('Staff.xml', XMLformated);
-
   }
-
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
-
   // Re-direct the browser back to the page, where the POST request came from
   res.redirect('back');
-
 });
 
 
-// POST request to add to JSON & XML files
-
+// POST request to delete a record
 router.post('/delete/json', function(req, res) {
-
-
   // Function to read in a JSON file, add to it & convert to XML
-
   function deleteEntryJSON(obj) {
-
     // Read in a JSON file
-
     var JSONfile = fs.readFileSync('Staff.json', 'utf8');
-
-
     // Parse the JSON file in order to be able to edit it 
-
     var JSONparsed = JSON.parse(JSONfile);
-
-
-
     //  JSONparsed.person.splice(JSONparsed.person.length-1, 1);
-
-
-
     //Removing a record from parsed json file using splice() 
-
     var index = -1;
-
     for (var i = 0; i != JSONparsed.member.length; i++) {
-
       if (JSONparsed.member[i].StaffId === obj.StaffId) {
-
         index = i;
-
       }
-
     }
-
     if (index != -1) {
-
       JSONparsed.member.splice(index, 1);
-
     }
-
-
-
     var JSONformated = JSON.stringify(JSONparsed, null, 4);
-
-
-
-
     // Write the updated JSON file back to the system 
-
     fs.writeFileSync('Staff.json', JSONformated);
-
-
-
-
     // Convert the updated JSON file to XML     
-
     var XMLformated = js2xmlparser.parse("staff", JSON.parse(JSONformated));
-
-
-
-
     // Write the resulting XML back to the system
-
     fs.writeFileSync('Staff.xml', XMLformated);
-
-
-
   }
-
-
-
-
   // Call appendJSON function and pass in body of the current POST request
-
   deleteEntryJSON(req.body);
-
-
-
   // Re-direct the browser back to the page, where the POST request came from
-
   res.redirect('back');
-
-
-
-
 });
-
-
-
-
 
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
